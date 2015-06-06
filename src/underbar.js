7e8c6bf2c8f85e3fa7c35a6769b3b061012589ab
Alex Leonetti <leonetti.tech@gmail.com>
2015-06-05 20:48:59 -0700

    Completed reject function passing the test cases. Was able to re-use filter function.

--- src/underbar.js
+++ src/underbar.js
@@ -98,9 +98,12 @@
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
-    // TIP: see if you can re-use _.filter() here, without simply
-    // copying code in and modifying it
+  		return _.filter(collection, function(val){
+  			return !test(val);
+  		});
   };
+  // TIP: see if you can re-use _.filter() here, without simply
+  // copying code in and modifying it
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
